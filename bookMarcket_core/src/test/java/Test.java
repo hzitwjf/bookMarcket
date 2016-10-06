951aa39e631ab1f278f5b430897a47e625e1d50d
wujiafeng <1364291059@qq.com>
2016-10-06 16:17:36 +0800

    测试

---
+++ bookMarcket_core/src/test/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ bookMarcket_web/src/test/java/TestServicesIpml/TestUserServicesIpml.java
@@ -0,0 +1,7 @@
+package TestServicesIpml;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class TestUserServicesIpml {
+}
