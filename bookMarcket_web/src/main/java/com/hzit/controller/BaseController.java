406151bff00b50c979a1bfa36a6b8ce982cbc9c8
晓峰 <1364291059@qq.com>
2016-10-07 13:53:12 +0800

    本地提交

---
+++ bookMarcket_web/src/main/java/com/hzit/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.hzit.controller;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Administrator on 2016/10/7.
+ */
+public class BaseController extends WebMvcConfigurerAdapter {
+
+}
---
+++ bookMarcket_web/src/main/java/com/hzit/interceptor/comment.java
@@ -0,0 +1,28 @@
+package com.hzit.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2016/10/7.
+ */
+public class comment implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
