88d0792ed3238e200940c5f801ca470ace910909
晓峰 <1364291059@qq.com>
2016-10-07 19:31:06 +0800

    本地提交

--- bookMarcket_web/src/main/java/com/hzit/controller/ShopcarController.java
+++ bookMarcket_web/src/main/java/com/hzit/controller/ShopcarController.java
@@ -2,6 +2,7 @@ package com.hzit.controller;
 
 import com.hzit.dao.entity.Book;
 import com.hzit.dao.vo.BookVo;
+import com.hzit.interceptor.comment;
 import com.hzit.services.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,7 +19,7 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/shop")
-public class ShopcarController {
+public class ShopcarController extends comment {
      @Autowired
       private BookServices bookServices;
 
