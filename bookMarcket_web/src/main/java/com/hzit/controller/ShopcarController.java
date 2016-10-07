9a58a420b17c047a14560c2e69a7260856037a91
maxiang <1677569741@qq.com>
2016-10-07 14:38:42 +0800

    book查询所有

--- bookMarcket_web/src/main/java/com/hzit/controller/ShopcarController.java
+++ bookMarcket_web/src/main/java/com/hzit/controller/ShopcarController.java
@@ -2,17 +2,22 @@ package com.hzit.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Administrator on 2016/10/7.
  */
 @Controller
+@RequestMapping("/shop")
 public class ShopcarController {
 
     @RequestMapping("/shopcar")
     @ResponseBody
-    public  Object shopcar(){
+    public  Object shopcar(@RequestParam("bookId") String[] bookId ){
+       for(int i=0;i<bookId.length;i++){
+           System.out.println(bookId[i]);
+       }
         System.out.println("购物车正在启动");
         return  "Ok";
     }
