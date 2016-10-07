1f4be23da79c839eb5c80cf5a44e94f107ebbcd3
maxiang <1677569741@qq.com>
2016-10-07 16:42:47 +0800

    book查询所有

--- bookMarcket_web/src/main/java/com/hzit/controller/ShopcarController.java
+++ bookMarcket_web/src/main/java/com/hzit/controller/ShopcarController.java
@@ -1,11 +1,16 @@
 package com.hzit.controller;
 
 import com.hzit.dao.entity.Book;
+import com.hzit.dao.vo.BookVo;
 import com.hzit.services.BookServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/10/7.
@@ -16,15 +21,27 @@ public class ShopcarController {
         private BookServices bookServices;
     @RequestMapping("/shopcar")
     @ResponseBody
-    public  Object shopcar(@RequestParam("bookId") String[] bookId ){
+    public  Object shopcar(@RequestParam("bookId") String[] bookId,HttpSession session){
 
         System.out.println("购物车正在启动");
         //通过bookid获取book对象
+          Map map=(Map)session.getAttribute("shopcar");
+        if(map==null){
+            map=new HashMap();
+        }
         for(int i=0;bookId!=null && i<bookId.length;i++ ){
             String  currentbookId=bookId[i];
             Book book=bookServices.findById(currentbookId);
+            BookVo bookVo=new BookVo();
+            bookVo.setBookAuthor(book.getBookAuthor());
+            bookVo.setBookId(book.getBookId());
+            bookVo.setBookStock(book.getBookStock());
+            bookVo.setBookPrice(book.getBookPrice());
+            bookVo.setBookPicUrl(book.getBookPicUrl());
+            bookVo.setShopCount(1);
+            map.put(bookVo.getBookId(),bookVo);
      }
-        return  "Ok";
+        return  "map";
     }
 
 
